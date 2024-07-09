814b3a667c595039f72d620fe5e26514106e2877
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-10 01:01:07 +0530

    Footer complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+        <html lang="en">
+            <body className={font.className}>
+                <Providers>
+                    {children}
+                </Providers>
+            </body>
+        </html>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,63 @@
+import {Button} from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import CallMadeOutlinedIcon from '@mui/icons-material/CallMadeOutlined';
+import MarkEmailReadOutlinedIcon from '@mui/icons-material/MarkEmailReadOutlined';
+
+export default function Footer(){
+    return(
+        <div className="bg-gradient-to-r from-violet-200 to-pink-200 ">
+            {/* Grid Layout */}
+            <div className="flex justify-center w-screen">
+                <div className="w-2/3 my-6 grid grid-cols-4 gap-4">
+                    <div className="col-span-1">
+                        <div 
+                            className="text-3xl font-semibold text-[#2E0C25]"
+                        >
+                            Gallery
+                        </div>
+                        <div>Events</div>
+                        <div>Blogs</div>
+                        <div>Newsletter</div>
+                    </div>
+                    <div className="col-span-2">
+                        <div 
+                            className="text-3xl font-semibold text-[#2E0C25]"
+                        >
+                            Team
+                        </div>
+                        <div>About Us</div>
+                        <div>Timeline</div>
+                        <div>Contact Us</div>
+                    </div>
+                    <div className="col-span-1">
+                        <div className="text-3xl font-semibold text-[#2E0C25]">Newsletter</div>
+                        <div className="my-2 text-[#2E0C25]">
+                            Never miss an opportunity! Subscribe to our 
+                            newsletter for upcoming events, member spotlights 
+                            and exclusive content.
+                        </div>
+                        <Input 
+                            type="email"
+                            placeholder="you@example.com"
+                            className="my-2 rounded-sm text-[#2E0C25]"
+                            endContent={<MarkEmailReadOutlinedIcon />}
+                        />
+                        <Button
+                            className="my-2 rounded-md bg-[#2E0C25] text-white"
+                            endContent={<CallMadeOutlinedIcon />}
+                        >
+                            Subscribe Now
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            {/* Underline */}
+            <hr className="w-3/5 mx-auto my-2 border-1 border-[#2E0C25]"/>
+            <div className="flex justify-center py-2 text-md">
+                <span className="text-[#2E0C25] mb-3">
+                    2024 Amrita Centre for Entrepreneurship. All rights reserved.
+                </span>
+            </div>
+        </div>
+    )
+}
