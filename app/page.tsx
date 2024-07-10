75d301bd535fdb8180de8c141e9dc46576507d76
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-10 15:37:52 +0530

    Add: FAQ Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
 import Banner from "@/components/Banner";
+import FAQSection from "@/components/Faq";
 import Footer from "@/components/Footer";
+
+const faqs = [
+    { 
+        question: "Can I modify or cancel my registration ?", 
+        answer: "Lorem ipsum dolor sit amet, qui minim labore adipisicing \
+        minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit \
+        amet, qui minim labore adipisicing minim sint cillum sint consectetur \
+        cupidatat."
+    },
+    { 
+        question: "How can I participate for breakout talks ?", 
+        answer: "Lorem ipsum dolor sit amet, qui minim labore adipisicing \
+        minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit \
+        amet, qui minim labore adipisicing minim sint cillum sint consectetur \
+        cupidatat."
+    },
+    { 
+        question: "Can I participate as sponsor of the event ?", 
+        answer: "Lorem ipsum dolor sit amet, qui minim labore adipisicing \
+        minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit \
+        amet, qui minim labore adipisicing minim sint cillum sint consectetur \
+        cupidatat."
+    },
+    { 
+        question: "Can I requeust for accomodation to attend the event ?", 
+        answer: "Lorem ipsum dolor sit amet, qui minim labore adipisicing \
+        minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit \
+        amet, qui minim labore adipisicing minim sint cillum sint consectetur \
+        cupidatat."
+    },
+    { 
+        question: "I have additional questions which were not covered here, \
+        whom can I contact ?", 
+        answer: "Lorem ipsum dolor sit amet, qui minim labore adipisicing \
+        minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit \
+        amet, qui minim labore adipisicing minim sint cillum sint consectetur \
+        cupidatat."
+    }
+]
 
 export default function Home() {
   return (
@@ -10,10 +50,7 @@ export default function Home() {
             display={true}
         />
         <div className="text-center py-20">
-            <b>Banner </b>
-                and
-            <b> Footer </b>
-                complete.
+            <FAQSection items={faqs}/>
         </div>
         <Footer />
     </div>
---
+++ components/Faq.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { Chip } from "@nextui-org/react";
+
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+
+type FAQ = {
+    question: string;
+    answer: string;
+}
+
+type FAQProps = {
+    items: FAQ[];
+}
+
+export default function FAQSection({ items }: FAQProps){
+    return(
+        <div className="w-full flex justify-center">
+            <div className="w-3/5">
+                <Accordion      
+                    motionProps={{
+                        variants: {
+                            enter: {
+                                y: 0,
+                                opacity: 1,
+                                height: "auto",
+                                transition: {
+                                    height: {
+                                        type: "spring",
+                                        stiffness: 500,
+                                        damping: 30,
+                                        duration: 1,
+                                    },
+                                    opacity: {
+                                        easings: "ease",
+                                        duration: 1,
+                                    },
+                                },
+                            },
+                            exit: {
+                                y: -10,
+                                opacity: 0,
+                                height: 0,
+                                transition: {
+                                    height: {
+                                        easings: "ease",
+                                        duration: 0.25,
+                                    },
+                                    opacity: {
+                                        easings: "ease",
+                                        duration: 0.3,
+                                    },
+                                },
+                            },
+                        },
+                    }}
+                    variant="light"
+                    className="flex flex-col gap-0"
+                >
+                    {items.map((item, index) => (
+                        <AccordionItem 
+                            key={index} 
+
+                            title={<div className="text-center flex items-center">
+                                <Chip variant="bordered" 
+                                    size="sm"
+                                    className="border-1 text-[#2E0C25] 
+                                    border-[#2E0C25] text-sm mx-2"
+                                >
+                                    {"0" + (index + 1)}
+                                </Chip>
+                                <span>
+                                    {item.question}
+                                </span>
+                            </div>}
+
+                            className="border-1 border-[#2E0C25] rounded-md p-2 font-semibold text-left"
+                            indicator={<KeyboardDoubleArrowLeftIcon style={{ color: "#2E0C25"}}/>}
+                        >
+                            <span
+                                className="font-normal px-2"
+                            >
+                                {item.answer}
+                            </span>
+                        </AccordionItem>
+                    ))}
+                </Accordion>
+            </div>
+        </div>
+    );
+}
