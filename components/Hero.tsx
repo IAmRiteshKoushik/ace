c5dff30878a96eaa768d175fdafd681dcfd89db3
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-08-04 22:35:25 +0530

    Hero section completed

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function HeroSection(){
+    return(
+        <div className="p-10 w-2/3 h-5/6">
+            <div className="w-full rounded-lg text-[#230C25] border-2 
+                border-[#230C25] bg-white p-4 flex flex-col items-center
+                shadow-2xl">
+                <div className="text-7xl">Amrita Centre for</div>  
+                <div className="text-9xl italic">Entrepreneurship</div>
+                <p className="text-4xl italic text-right w-4/5 pt-3">Edition: 2023-24</p>
+            </div>
+        </div>
+    );
+}
