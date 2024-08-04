a693ff444bba6ae5f620c12efe7eb7255e4ed71b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-08-05 01:11:00 +0530

    Checkpoint commit

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,5 +1,3 @@
-import { Image } from "@nextui-org/react";
-
 export default function Gallery(){
     return(
         <div className="w-3/4">
@@ -7,62 +5,6 @@ export default function Gallery(){
                 className="grid grid-cols-4 gap-6 rounded-lg border-2 
                 border-[#230C25] "
             >
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
-                <div>
-                    <Image
-                        isZoomed
-                        src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-                        alt="Gallery Image"
-                        className="w-[240px]"
-                    />
-                </div>
             </div>
         </div>
     );
