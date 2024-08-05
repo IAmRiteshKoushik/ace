a5fc771678c02da0b572c25010310476f885cc5a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-08-06 00:29:14 +0530

    Add: Gradient to Gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -3,9 +3,21 @@ import Marquee from "react-fast-marquee";
 
 export default function Gallery() {
     return (
-        <div className="relative -z-10">
+        <div className="relative -z-10 flex flex-col items-center
+            bg-gradient-to-r from-violet-200 to-pink-200">
+            <div className="w-3/5 text-[#230C25] font-semibold text-3xl mt-4">Join Our Thriving Community (Image Gallery)</div>
             <Marquee pauseOnHover speed={70} direction={"left"}>
                 <div className="flex justify-center">
+                    <div className="m-5">
+                        <Image
+                            src="/sponsor_1.png"
+                            alt="Gallery Image 1"
+                            className="w-[240px]"
+                            width={200}
+                            height={500}
+
+                        />
+                    </div>
                     <div className="m-5">
                         <Image
                             src="/sponsor_1.png"
@@ -125,6 +137,15 @@ export default function Gallery() {
                             height={500}
 
 
+                        />
+                    </div>
+                    <div className="m-5">
+                        <Image
+                            src="/sponsor_1.png"
+                            alt="Gallery Image 4"
+                            className="w-[240px]"
+                            width={200}
+                            height={500}
                         />
                     </div>
                     <div className="m-5">
